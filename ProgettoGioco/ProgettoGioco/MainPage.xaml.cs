9904c4816f2303ccbded24a00c7176cb4d13a3b9
Llamafinance <lamafinance001@gmail.com>
2021-06-01 11:40:04 +0200

    Add project files.

---
+++ ProgettoGioco/ProgettoGioco/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProgettoGioco
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //da modificare dal gruppo 1
+            //Navigation.PushAsync(new  )
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            //da modificare dal gruppo 2
+            //Navigation.PushAsync(new  )
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            //da modificare dal gruppo 3
+            //Navigation.PushAsync(new  )
+        }
+    }
+}
