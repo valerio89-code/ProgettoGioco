c27e783886da5471ac17197507eee0d9f582841c
luaretino1998@gmail.com <luaretino1998@gmail.com>
2021-06-01 17:53:50 +0200

    Aggiunto bottone (exit),  creato metodo (verifica) Creata sequenza random

--- ProgettoGioco/ProgettoGioco/Gioco2/Gioco2.xaml.cs
+++ ProgettoGioco/ProgettoGioco/Gioco2/Gioco2.xaml.cs
@@ -12,9 +12,38 @@ namespace ProgettoGioco.Gioco2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Gioco2 : ContentPage
     {
-        public Gioco2()
+        public int[] Sequenza { get; set; }
+        public int CifreSequenza { get; set; }
+        public Gioco2(int livello)
         {
             InitializeComponent();
+            switch (livello)
+            {
+                case 1:
+                    CifreSequenza = 4;
+                    break;
+                case 2:
+                    CifreSequenza = 5;
+                    break;
+                case 3:
+                    CifreSequenza = 6;
+                    break;
+                case 4:
+                    CifreSequenza = 7;
+                    break;
+                case 5:
+                    CifreSequenza = 8;
+                    break;
+
+                default: CifreSequenza = 9;
+                    break;
+            }
+            Sequenza = new int[CifreSequenza];
+            for (int i = 0; i < CifreSequenza; i++)
+            {
+                Random rnd = new Random();
+                Sequenza[i] = rnd.Next(0, 9);
+            }
         }
 
         private void btn_uno_Clicked(object sender, EventArgs e)
@@ -65,6 +94,15 @@ namespace ProgettoGioco.Gioco2
         private void btn_zero_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_exit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private bool VerificaInput(int numInserito, int numSequenza)
+        {
+            return numInserito == numSequenza;
         }
     }
 }
