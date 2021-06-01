07854fa2492ed4984102cbff48facef61905c717
Llamafinance <lamafinance001@gmail.com>
2021-06-01 11:48:31 +0200

    Aggiunte pagine di default vari giochi

---
+++ ProgettoGioco/ProgettoGioco/Gioco3/Gioco3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProgettoGioco.Gioco3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Gioco3 : ContentPage
+    {
+        public Gioco3()
+        {
+            InitializeComponent();
+        }
+    }
+}
