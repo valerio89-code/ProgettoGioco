7ede16f48d44af6092fbca181bd31c4f453f81c8
Luca Melis <luca.melis9832111@gmail.com>
2021-06-02 17:13:04 +0200

    aggiunto la tempistica dello spown del bottone

--- ProgettoGioco/ProgettoGioco/Gioco1/StartGameOne.xaml.cs
+++ ProgettoGioco/ProgettoGioco/Gioco1/StartGameOne.xaml.cs
@@ -38,18 +38,28 @@ namespace ProgettoGioco.Gioco1
 
         private void lbl_tempoStart_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
+            var numeroRandom = rnd.Next(1,10);
+            var numeroRandom2 = rnd.Next(1,10);
             if (lbl_tempoStart.Text == "10")
             {
                 Navigation.PopAsync();
             }
-            if (lbl_tempoStart.Text == "1" || lbl_tempoStart.Text == "2" || lbl_tempoStart.Text == "3" || lbl_tempoStart.Text == "4")
+            if (lbl_tempoStart.Text == numeroRandom.ToString() || lbl_tempoStart.Text == numeroRandom2.ToString())
             {
-                btn_CliccaImmagine.IsVisible = false;
                 btn_CliccaImmagine.TranslationY = rnd.Next(-190, 190);
                 btn_CliccaImmagine.TranslationX = rnd.Next(-190, 190);
+                btn_CliccaImmagine.IsVisible = true;
+                TempoAcceso.Start();
             }
-            btn_CliccaImmagine.IsVisible = true;
+            if (TempoAcceso.Elapsed.TotalSeconds >= 0.5)
+            {
+                btn_CliccaImmagine.IsVisible = false;
+                TempoAcceso.Stop();
+            }
+        }
 
+        private void btn_CliccaImmagine_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
 
         }
     }
