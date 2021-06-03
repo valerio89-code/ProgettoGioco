33e63e227e0f40d3f27aca5424e2b791558bcb9e
AzarielPie <azarielgaming97@gmail.com>
2021-06-03 22:19:57 +0200

    Modifiche varie al codice Aggiunta scomparsa orata quando viene cliccata Modificato spostamento automatico ogni secondo

--- ProgettoGioco/ProgettoGioco/Gioco1/StartGameOne.xaml.cs
+++ ProgettoGioco/ProgettoGioco/Gioco1/StartGameOne.xaml.cs
@@ -28,7 +28,7 @@ namespace ProgettoGioco.Gioco1
         private void btn_CliccaImmagine_Clicked(object sender, EventArgs e)
         {
             Hit++;
-            
+            btn_CliccaImmagine.IsVisible = false;
         }
 
         private void ImageButton_Clicked(object sender, EventArgs e)
@@ -38,24 +38,31 @@ namespace ProgettoGioco.Gioco1
 
         private void lbl_tempoStart_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            var numeroRandom = rnd.Next(1,10);
-            var numeroRandom2 = rnd.Next(1,10);
             if (lbl_tempoStart.Text == "10")
             {
                 Navigation.PopAsync();
             }
-            if (lbl_tempoStart.Text == numeroRandom.ToString() || lbl_tempoStart.Text == numeroRandom2.ToString())
-            {
-                btn_CliccaImmagine.TranslationY = rnd.Next(-190, 190);
-                btn_CliccaImmagine.TranslationX = rnd.Next(-190, 190);
-                btn_CliccaImmagine.IsVisible = true;
-                TempoAcceso.Start();
-            }
-            if (TempoAcceso.Elapsed.TotalSeconds >= 0.5)
-            {
-                btn_CliccaImmagine.IsVisible = false;
-                TempoAcceso.Stop();
-            }
+            btn_CliccaImmagine.TranslationY = rnd.Next(-190, 190);
+            btn_CliccaImmagine.TranslationX = rnd.Next(-190, 190);
+            btn_CliccaImmagine.IsVisible = true;
+            //var numeroRandom = rnd.Next(1,10);
+            //var numeroRandom2 = rnd.Next(1,10);
+            //if (lbl_tempoStart.Text == "10")
+            //{
+            //    Navigation.PopAsync();
+            //}
+            //if (lbl_tempoStart.Text == numeroRandom.ToString() || lbl_tempoStart.Text == numeroRandom2.ToString())
+            //{
+            //    btn_CliccaImmagine.TranslationY = rnd.Next(-190, 190);
+            //    btn_CliccaImmagine.TranslationX = rnd.Next(-190, 190);
+            //    btn_CliccaImmagine.IsVisible = true;
+            //    TempoAcceso.Start();
+            //}
+            //if (TempoAcceso.Elapsed.TotalSeconds >= 0.5)
+            //{
+            //    btn_CliccaImmagine.IsVisible = false;
+            //    TempoAcceso.Stop();
+            //}
         }
 
         private void btn_CliccaImmagine_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
